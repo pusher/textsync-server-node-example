d9b96a64b057362d3376737c1d31b595ab956fc6
James Lees <james@lees.io>
2018-02-20 15:56:29 +0000

    Usability tweaks (#3)
    * add dynamic endpoint and homepage
    
    * Add homepage to auth server with details

---
+++ app/html-generator.js
@@ -0,0 +1,30 @@
+const generate = endpoint => {
+  return `<html>
+  <head>
+    <title>TextSync Auth Server Example</title>
+  </head>
+  <body>
+    <h1>TextSync Auth Server Example</h1>
+    <p>
+      Your TextSync example auth server is up.
+      Your <code>authEndpoint</code> is:
+    </p>
+      <pre>
+<span id="host"></span>${endpoint}
+      </pre>
+    <p>Try sending an example <code>POST</code> request  using <code>curl</code>
+    <pre>
+curl -X POST <span id="host"></span>${endpoint} \\
+  -H "Content-Type: application/json" \\
+  -d '{"docId": "example document"}'
+    </pre>
+  </body>
+  <script>
+    document
+      .querySelectorAll('#host')
+      .forEach(node => {node.innerText = window.location.origin})
+  </script>
+</html>`;
+};
+
+exports.generate = generate;

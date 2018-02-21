10e50d055914dbbc17e944b5bb5562b54ed9a659
James Lees <james.lees@pusher.com>
2018-02-21 13:48:36 +0000

    Rewite without express dependency

--- app/server.js
+++ app/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const TextSync = require('textsync-server-node');
 const config = require('./config');
 const htmlGenerator = require('./html-generator');
+const http = require('http');
+const url = require('url');
 
-const app = express();
 const port = config.PORT || 3030;
 const textsync = new TextSync({
   instanceLocator: config.INSTANCE_LOCATOR,
   key: config.KEY
 });
+const html = htmlGenerator.generate(config.ENDPOINT);
 
 const debug = (...args) => {
   if (config.DEBUG) {
@@ -17,52 +17,99 @@ const debug = (...args) => {
   }
 };
 
-/* Allow CORS */
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const hasJSONHeader = headers => {
+  let contentType = headers['content-type'];
+  if (!contentType) return false;
+  return contentType.split(';')[0] === 'application/json';
+};
 
-app.post(config.ENDPOINT, (req, res) => {
-  debug('received auth request');
-  debug('req.body:', req.body);
-  // The permissions function should take information about the user from the
-  // req object and determine the permissions the user has for the requested
-  // document.
-  //
-  // The users permissions should be modelled as an array of the permissions
-  // exposed by TextSync.Permissions. The permissions function should return
-  // a promise resolving to this array.
-  //
-  // For simplicity, this permissions function is granting full READ/WRITE
-  // permissions to all requests
-  const permissionsFn = documentId => {
-    return Promise.resolve([
-      TextSync.Permissions.READ,
-      TextSync.Permissions.WRITE
-    ]);
-  };
+const parseJSONBody = req => {
+  let body = '';
+  return new Promise((resolve, reject) => {
+    req
+      .on('data', chunk => {
+        body += chunk.toString();
+      })
+      .on('end', () => {
+        let json;
+        try {
+          json = JSON.parse(body);
+        } catch (e) {
+          reject(e);
+        }
+        resolve(json);
+      });
+  });
+};
 
-  // set the tokenExpiry to 20 minutes
-  let options = { tokenExpiry: 1 * 60 * 20 };
-  let token = textsync
-    .authorizeDocument(req.body, permissionsFn, options)
-    .then(token => {
-      res.json(token);
-    });
-});
+http
+  .createServer((req, res) => {
+    // Allow CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization'
+    );
 
-app.get('/', (req, res) => {
-  let html = htmlGenerator.generate(config.ENDPOINT);
-  res.set('Content-Type', 'text/html; charset=utf-8');
-  res.send(html);
-});
+    const { method, headers } = req;
+    const _url = req.url;
+    let path = url.parse(_url).pathname;
+    if (
+      method !== 'POST' ||
+      !hasJSONHeader(headers) ||
+      path !== config.ENDPOINT
+    ) {
+      res.setHeader('Content-Type', 'text/html');
+      res.write(html);
+      res.end();
+      return;
+    }
+    parseJSONBody(req)
+      .then(body => {
+        debug('received auth request');
+        debug('req.body:', req.body);
+        // The permissions function should take information about the user from the
+        // req object and determine the permissions the user has for the requested
+        // document.
+        //
+        // The users permissions should be modelled as an array of the permissions
+        // exposed by TextSync.Permissions. The permissions function should return
+        // a promise resolving to this array.
+        //
+        // For simplicity, this permissions function is granting full READ/WRITE
+        // permissions to all requests
+        const permissionsFn = documentId => {
+          return Promise.resolve([
+            TextSync.Permissions.READ,
+            TextSync.Permissions.WRITE
+          ]);
+        };
 
-app.listen(port, () => {
-  let msg = `listening on ${port}`;
-  if (config.DEBUG) msg += ' - DEBUG mode';
-  console.log(msg);
-});
+        // set the tokenExpiry to 20 minutes
+        let options = { tokenExpiry: 1 * 60 * 20 };
+        let token = textsync
+          .authorizeDocument(body, permissionsFn, options)
+          .then(token => {
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify(token));
+            res.end();
+          })
+          .catch(err => {
+            console.error('Error, authorizing document', err);
+            res.statusCode = 500;
+            res.write('Internal Server Error');
+            res.end();
+          });
+      })
+      .catch(err => {
+        console.error('Error parsing json', err);
+        res.statusCode = 400;
+        res.write('Malformed JSON');
+        res.end();
+      });
+  })
+  .listen(port, () => {
+    let msg = `listening on ${port}`;
+    if (config.DEBUG) msg += ' - DEBUG mode';
+    console.log(msg);
+  });
